3d4875579a397b6c2c405bfa7abcd61a6584e079
Chrstn67 <chrstn.hmbrt67.com>
2023-11-23 08:52:21 +0100

    grid composant gastronomie

--- src/components/Gastronomie/Gastronomie.jsx
+++ src/components/Gastronomie/Gastronomie.jsx
@@ -2,20 +2,25 @@ import React from "react";
 import "./Gastronomie.scss";
 
 const foodData = [
-  { name: "Kougelhopf", image: "Kougelhopf.png" },
-  { name: "Bredele", image: "Bredele.png" },
-  { name: "Mannele", image: "Mannele.png" },
-  { name: "Schwartzwalder", image: "Schwartzwalder.png" },
-  { name: "Pains d'épice", image: "Pain_epice.png" },
-  { name: "Steutzel", image: "Steutzel.png" },
-  { name: "Flammenkuche", image: "Flammenkuche.png" },
+  { name: "Choucroute", image: "Choucroute.png", category: "salé" },
+  { name: "Flammenkuche", image: "Flammenkuche.png", category: "salé" },
+  { name: "Knepfle", image: "Knepfle.png", category: "salé" },
+  { name: "Baeckeoffe", image: "Baeckeoffe.png", category: "salé" },
+  { name: "Fleischschnacka", image: "Fleischschnacka.png", category: "salé" },
+  { name: "Presskopf", image: "Presskopf.png", category: "salé" },
+  { name: "Munster", image: "Munster.png", category: "salé" },
 
-  { name: "Knepfle", image: "Knepfle.png" },
-  { name: "Baeckeoffe", image: "Baeckeoffe.png" },
-  { name: "Fleischschnacka", image: "Fleischschnacka.png" },
-  { name: "Presskopf", image: "Presskopf.png" },
-  { name: "Munster", image: "Munster.png" },
-  { name: "Choucroute", image: "Choucroute.png" },
+  { name: "Kougelhopf", image: "Kougelhopf.png", category: "sucré" },
+  { name: "Bredele", image: "Bredele.png", category: "sucré" },
+  { name: "Mannele", image: "Mannele.png", category: "sucré" },
+  { name: "Schwarzwalder", image: "Schwartzwalder.png", category: "sucré" },
+  { name: "Pains d'épice", image: "Pain_epice.png", category: "sucré" },
+  { name: "Steutzel", image: "Steutzel.png", category: "sucré" },
+  {
+    name: "Tarte aux questches",
+    image: "Tarte_questches.png",
+    category: "sucré",
+  },
 ];
 
 const Gastronomie = () => {
@@ -23,24 +28,46 @@ const Gastronomie = () => {
     <>
       <section className="food">
         <div>
-          <h3>Qu&#39;est-ce qu&#39;on mange à midi ?</h3>
+          <h3>e Gueter !</h3>
           <p>Savourez vos pauses en dégustant nos spécialités régionales !</p>
         </div>
         <div className="picture-food">
-          <ul className="word-cloud">
-            {foodData.map((foodItem, index) => (
-              <li key={index} className="food-list">
-                <img
-                  src={`./images/Gastronomie/${foodItem.image}`}
-                  alt={foodItem.name}
-                />
-                <p>{foodItem.name}</p>
-              </li>
-            ))}
-          </ul>
+          <section className="category-container column">
+            <h4 className="category-title">Salé</h4>
+            <ul className="word-cloud">
+              {foodData
+                .filter((foodItem) => foodItem.category === "salé")
+                .map((foodItem, index) => (
+                  <li key={`sale-${index}`} className="food-list">
+                    <img
+                      src={`./images/Gastronomie/${foodItem.image}`}
+                      alt={foodItem.name}
+                    />
+                    <p>{foodItem.name}</p>
+                  </li>
+                ))}
+            </ul>
+          </section>
+          <section className="category-container column">
+            <h4 className="category-title">Sucré</h4>
+            <ul className="word-cloud">
+              {foodData
+                .filter((foodItem) => foodItem.category === "sucré")
+                .map((foodItem, index) => (
+                  <li key={`sucre-${index}`} className="food-list">
+                    <img
+                      src={`./images/Gastronomie/${foodItem.image}`}
+                      alt={foodItem.name}
+                    />
+                    <p>{foodItem.name}</p>
+                  </li>
+                ))}
+            </ul>
+          </section>
         </div>
       </section>
     </>
   );
 };
+
 export default Gastronomie;
